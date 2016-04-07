9a6c41dde9037ee7245bc22b77b6376961cdf52f
DennisSchnipper <schnipperdennis@gmail.com>
2016-04-07 10:15:32 +0200

    profielafbeeldinguploaden toegevoegd aan aanmeldscherm UI

--- Participation/Aanmeldscherm.Designer.cs
+++ Participation/Aanmeldscherm.Designer.cs
@@ -36,18 +36,21 @@
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -132,7 +135,7 @@
             this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Enabled = false;
-            this.groupBox1.Location = new System.Drawing.Point(18, 193);
+            this.groupBox1.Location = new System.Drawing.Point(18, 214);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(594, 64);
             this.groupBox1.TabIndex = 7;
@@ -140,15 +143,31 @@
             this.groupBox1.Text = "Registratie Hulpverlener";
             this.groupBox1.Visible = false;
             // 
-            // label6
+            // button1
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(13, 115);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(160, 25);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "Geboortedatum";
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(508, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 20);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "bladeren...";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(186, 16);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(316, 20);
+            this.textBox6.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(7, 40);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(156, 13);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Geaccepteerde formats zijn: .txt";
             // 
             // label7
             // 
@@ -160,14 +179,15 @@
             this.label7.TabIndex = 4;
             this.label7.Text = "Verklaring goed gedrag";
             // 
-            // label8
+            // label6
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(7, 40);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(156, 13);
-            this.label8.TabIndex = 5;
-            this.label8.Text = "Geaccepteerde formats zijn: .txt";
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(13, 115);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(160, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Geboortedatum";
             // 
             // textBox1
             // 
@@ -204,28 +224,11 @@
             this.textBox5.Size = new System.Drawing.Size(375, 20);
             this.textBox5.TabIndex = 13;
             // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(186, 16);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(316, 20);
-            this.textBox6.TabIndex = 14;
-            // 
-            // button1
-            // 
-            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(508, 16);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(69, 20);
-            this.button1.TabIndex = 15;
-            this.button1.Text = "bladeren...";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button2.Location = new System.Drawing.Point(17, 264);
+            this.button2.Location = new System.Drawing.Point(17, 280);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(112, 45);
             this.button2.TabIndex = 14;
@@ -236,19 +239,49 @@
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button3.Location = new System.Drawing.Point(441, 264);
+            this.button3.Location = new System.Drawing.Point(441, 280);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(171, 45);
             this.button3.TabIndex = 15;
             this.button3.Text = "Aanmelden";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(13, 188);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(173, 25);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Profielafbeelding";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(237, 193);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(303, 20);
+            this.textBox7.TabIndex = 17;
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(544, 192);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(69, 20);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "bladeren...";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Aanmeldscherm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(624, 321);
+            this.ClientSize = new System.Drawing.Size(624, 334);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox5);
@@ -267,7 +300,6 @@
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.MaximumSize = new System.Drawing.Size(640, 360);
             this.MinimumSize = new System.Drawing.Size(640, 360);
             this.Name = "Aanmeldscherm";
             this.Text = "Aanmeldscherm";
@@ -300,5 +332,8 @@
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button4;
     }
 }
