4314276934488761a3e24b587e63caa184e98709
Sven Henderickx <s.henderickx@student.fontys.nl>
2016-04-07 13:50:35 +0200

    user class aangemaakt + prop
    :+1:

---
+++ Participation/SharedModels/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation.SharedModels
+{
+    public class User
+    {
+        //Properties
+        public string Name { get; set; }
+        private string _email { get; set; }
+        public string Description { get; set; }
+        public DateTime Birthday { get; set; }
+        public string ProfilePicture { get; set; }
+        public string Location { get; set; }
+        private string _phoneNumber { get; set; }
+        public GenderEnum Gender { get; set; }
+        private List<Meeting> _meetings { get; set; }
+
+        //constructors
+        public User(string name, string email, string despription, DateTime birthday, string profilePicture, string location, string phoneNumber, GenderEnum gender)
+        {
+            this.Name = name;
+            this._email = email;
+            this.Description = despription;
+            this.Birthday = birthday;
+            this.ProfilePicture = profilePicture;
+            this.Location = location;
+            this._phoneNumber = phoneNumber;
+            this.Gender = gender;
+            _meetings = new List<Meeting>();
+        }
+    }
+}
