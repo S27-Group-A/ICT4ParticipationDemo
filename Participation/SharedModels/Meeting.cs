42207abca0450fa3a0eed4e8706f25a9691f633f
Sven Henderickx <s.henderickx@student.fontys.nl>
2016-04-14 14:43:59 +0200

    meeting gevuld

--- Participation/SharedModels/Meeting.cs
+++ Participation/SharedModels/Meeting.cs
@@ -1,11 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Participation.SharedModels
+﻿namespace Participation.SharedModels
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
     public class Meeting
     {
+        // constructor
+        public Meeting(Volunteer volunteer, Patient patient, DateTime date, string location)
+        {
+            this.Volunteer = volunteer;
+            this.Patient = patient;
+            this.Date = date;
+            this.Location = location;
+        }
+
+        // Properties
+
+        public Volunteer Volunteer { get; private set; }
+        public Patient Patient { get; private set; }
+        public DateTime Date { get; private set; }
+        public string Location { get; private set; }
+
+        // Methods
+
+        public bool AcceptMeeting()
+        {
+            return false;
+        }
     }
 }
