504ca416815dfa06d6ef0c976ed16eadb94664a2
TomRuijs <ruijs.tom@gmail.com>
2016-04-21 23:20:58 +0200

    Added a id to review

--- Participation/SharedModels/Review.cs
+++ Participation/SharedModels/Review.cs
@@ -7,6 +7,7 @@ namespace Participation.SharedModels
 {
     public class Review
     {
+        public int Id { get; set; }
         public double Rating { get; set; }
         public string Text { get; set; }
     }
