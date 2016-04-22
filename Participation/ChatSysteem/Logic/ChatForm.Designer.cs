3aaf1ec2b7f9936311bc0b113cbd162a56d8f819
Sanko-Kallig <adk_m91@hotmail.com>
2016-04-22 11:41:20 +0200

    ChatSystem - Untested form opening on new msg

--- Participation/ChatSysteem/Logic/ChatForm.Designer.cs
+++ Participation/ChatSysteem/Logic/ChatForm.Designer.cs
@@ -31,85 +31,59 @@
             this.lstClients = new System.Windows.Forms.ListBox();
             this.txtSend = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
-            this.txtMsgs = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtUserName = new System.Windows.Forms.TextBox();
-            this.btnLogin = new System.Windows.Forms.Button();
+            this.tbxMessage = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lstClients
             // 
             this.lstClients.FormattingEnabled = true;
-            this.lstClients.Location = new System.Drawing.Point(340, 31);
+            this.lstClients.ItemHeight = 16;
+            this.lstClients.Location = new System.Drawing.Point(453, 38);
+            this.lstClients.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstClients.Name = "lstClients";
-            this.lstClients.Size = new System.Drawing.Size(120, 199);
+            this.lstClients.Size = new System.Drawing.Size(159, 244);
             this.lstClients.TabIndex = 0;
             // 
             // txtSend
             // 
-            this.txtSend.Location = new System.Drawing.Point(84, 236);
+            this.txtSend.Location = new System.Drawing.Point(112, 290);
+            this.txtSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSend.Multiline = true;
             this.txtSend.Name = "txtSend";
-            this.txtSend.Size = new System.Drawing.Size(376, 51);
+            this.txtSend.Size = new System.Drawing.Size(500, 62);
             this.txtSend.TabIndex = 1;
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(1, 234);
+            this.btnSend.Location = new System.Drawing.Point(1, 288);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(83, 53);
+            this.btnSend.Size = new System.Drawing.Size(111, 65);
             this.btnSend.TabIndex = 2;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
-            // txtMsgs
+            // tbxMessage
             // 
-            this.txtMsgs.Location = new System.Drawing.Point(1, 31);
-            this.txtMsgs.Multiline = true;
-            this.txtMsgs.Name = "txtMsgs";
-            this.txtMsgs.Size = new System.Drawing.Size(333, 199);
-            this.txtMsgs.TabIndex = 3;
+            this.tbxMessage.Location = new System.Drawing.Point(1, 38);
+            this.tbxMessage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbxMessage.Multiline = true;
+            this.tbxMessage.Name = "tbxMessage";
+            this.tbxMessage.Size = new System.Drawing.Size(443, 244);
+            this.tbxMessage.TabIndex = 3;
             // 
-            // label1
+            // ChatForm
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1, 5);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "User Name:";
-            // 
-            // txtUserName
-            // 
-            this.txtUserName.Location = new System.Drawing.Point(70, 5);
-            this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(264, 20);
-            this.txtUserName.TabIndex = 5;
-            // 
-            // btnLogin
-            // 
-            this.btnLogin.Location = new System.Drawing.Point(340, 2);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(120, 24);
-            this.btnLogin.TabIndex = 6;
-            this.btnLogin.Text = "Login";
-            this.btnLogin.UseVisualStyleBackColor = true;
-            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
-            // 
-            // frmClient
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 299);
-            this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.txtUserName);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtMsgs);
+            this.ClientSize = new System.Drawing.Size(619, 368);
+            this.Controls.Add(this.tbxMessage);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.txtSend);
             this.Controls.Add(this.lstClients);
-            this.Name = "frmClient";
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ChatForm";
             this.Text = "Client";
             this.Load += new System.EventHandler(this.frmClient_Load);
             this.ResumeLayout(false);
@@ -122,10 +96,7 @@
         private System.Windows.Forms.ListBox lstClients;
         private System.Windows.Forms.TextBox txtSend;
         private System.Windows.Forms.Button btnSend;
-        private System.Windows.Forms.TextBox txtMsgs;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtUserName;
-        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox tbxMessage;
     }
 }
 
--- Participation/ChatSysteem/Logic/ChatUsers.cs
+++ Participation/ChatSysteem/Logic/ChatUsers.cs
@@ -25,21 +25,27 @@ namespace Participation.ChatSysteem
             rc.Start(rc, FormProvider.LoggedInUser);
 
             rc.NewNames += new GotNames(rc_NewNames);
+            rc.ReceiveMsg += new ReceivedMessage(rc_ReceiveMessage);
         }
 
         private void rc_NewNames(object sender, List<string> names)
         {
-            lstClients.Items.Clear();
+            lbxClients.Items.Clear();
             foreach (string name in names)
             {
                 if (name != FormProvider.LoggedInUser.Name)
-                    lstClients.Items.Add(name);
+                    lbxClients.Items.Add(name);
             }
         }
 
         private void btnStartChat_Click(object sender, EventArgs e)
         {
+            FormProvider.ChatForm(this.rc, lbx)
+        }
 
+        private void rc_ReceiveMessage(string sender, string msg)
+        {
+            FormProvider.ChatForm(this.rc, sender, msg);
         }
     }
 }
