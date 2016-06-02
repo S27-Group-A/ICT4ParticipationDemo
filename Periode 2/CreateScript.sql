fe09ab2d0f50a0eb8b45a6d1b29556e02246f36b
TomRuijs <ruijs.tom@gmail.com>
2016-06-02 13:16:13 +0200

    Added drop, create and foreign key constraints

---
+++ Periode 2/CreateScript.sql
@@ -0,0 +1,168 @@
+--DROP TABLES
+DROP TABLE "Account" CASCADE CONSTRAINTS;
+DROP TABLE "User" CASCADE CONSTRAINTS;
+DROP TABLE "Admin" CASCADE CONSTRAINTS;
+DROP TABLE Volunteer CASCADE CONSTRAINTS;
+DROP TABLE Skill CASCADE CONSTRAINTS;
+DROP TABLE Review CASCADE CONSTRAINTS;
+DROP TABLE Request CASCADE CONSTRAINTS;
+DROP TABLE VehicleType CASCADE CONSTRAINTS;
+DROP TABLE Patient CASCADE CONSTRAINTS;
+DROP TABLE Meeting CASCADE CONSTRAINTS;
+DROP TABLE VolunteerSkill CASCADE CONSTRAINTS;
+DROP TABLE RequestSkill CASCADE CONSTRAINTS;
+DROP TABLE Chat CASCADE CONSTRAINTS;
+DROP TABLE "Availability" CASCADE CONSTRAINTS;
+DROP TABLE Response CASCADE CONSTRAINTS;
+
+--CREATE TABLES AND ADD PRIMARY KEYS
+Create Table "Account"
+(
+  Accountid Number(10) Primary Key,
+  Username Varchar2(32) Not Null,
+  Password Varchar2(32) Not Null,
+  Email Varchar2(255) Not Null
+);
+
+Create Table "User"
+(
+  Accountid Number(10) Primary Key,
+  Name Varchar2(32) Not Null,
+  Phone Varchar2(16),
+  Datederegistration Date,
+  Adress Varchar2(255),
+  Location Varchar(255),
+  Car Varchar2(1),
+  Driverslicense Varchar2(1),
+  Rfid Varchar2(255),
+  Banned Varchar2(1),
+  Unban Date,
+  Enabled Varchar2(1)
+);
+
+Create Table "Admin"
+(
+  Accountid Number(10) Primary Key
+);
+
+
+Create Table Volunteer
+(
+  Accountid Number(10) Primary Key,
+  Vog Varchar2(255),
+  Birthdate Date,
+  Photo Varchar2(255),
+  Vogconfirmation Varchar2(1)
+);
+
+Create Table Skill
+(
+  Skillid Number(10) Primary Key,
+  Description Varchar2(255) Not Null
+);
+
+Create Table Request
+(
+  Requestid Number(10) Primary Key,
+  Description Varchar2(255),
+  Location Varchar2(255),
+  Traveltime Timestamp,
+  Startdate Date,
+  Enddate Date,
+  Urgency Number(1),
+  Amountofvolunteers Number(10)
+);
+
+Create Table Review
+(  
+  Reviewid Number(10),
+  Accountid Number(10),
+  Requestid Number(10),
+  Rating Number(2),
+  "Comment" Varchar2(255),
+  Primary Key (Reviewid, Accountid, Requestid)
+);
+
+Create Table Vehicletype
+( 
+  Vehicletypeid Number(10),
+  Requestid Number(10),
+  Description Varchar2(255) Not Null,
+  Primary Key (Vehicletypeid, Requestid)
+);
+
+Create Table Patient
+( 
+  Accountid Number(10) Primary Key,
+  Ov Varchar2(1)
+);
+
+Create Table Meeting
+(
+  Volunteerid Number(10),
+  Patientid Number(10),
+  Location Varchar(255),
+  Meetingdate Date,
+  Status Varchar2(1),
+  Primary Key(Volunteerid, Patientid)
+);
+
+Create Table Volunteerskill
+(
+  Accountid Number(10),
+  Skillid Number(10),
+  Primary Key (Accountid, Skillid)
+);
+--TODO Drop below
+Create Table Requestskill
+(
+  Requestid Number(10),
+  Skillid Number(10),
+  Primary Key (RequestId, Skillid)
+);
+
+Create Table Chat
+(
+  Accounta Number(10),
+  Accountb Number(10),
+  Datetime Date,
+  Message Varchar2(255) Not Null,
+  Primary Key (Accounta, Accountb, Datetime)
+);
+
+Create Table "Availability"
+(
+  Accountid Number(10) Primary Key,
+  Day Varchar2(2),
+  Timeofday Varchar2(10) 
+);
+
+Create Table Response
+(
+  ResponderId NUMBER(10),
+  RequestId NUMBER(10),
+  ResponseDate DATE,
+  Description VARCHAR2(255),
+  PRIMARY KEY (ResponderId, RequestId, ResponseDate)
+);
+
+--ADD FOREIGN KEY CONSTRAINTS
+Alter Table "User" Add Foreign Key (Accountid) References Account(Accountid);
+Alter Table "Admin" Add Foreign Key (Accountid) References Account(Accountid);
+Alter Table Volunteer Add Foreign Key (Accountid) References "User"(Accountid);
+Alter Table Patient Add Foreign Key (Accountid) References "User" (Accountid);
+Alter Table "Availability" Add Foreign Key (Accountid) References Volunteer(Accountid);
+Alter Table Meeting Add Foreign Key (Volunteerid) References Volunteer(Accountid);
+Alter Table Meeting Add Foreign Key (Patientid) References Patient(Accountid);
+Alter Table Chat Add Foreign Key (Accounta) References Volunteer(Accountid);
+Alter Table Chat Add Foreign Key (Accountb) References Patient(Accountid);
+Alter Table Volunteerskill Add Foreign Key (Accountid) References Volunteer(Accountid);
+Alter Table Volunteerskill Add Foreign Key (Skillid) References Skill(Skillid);
+Alter Table Requestskill Add Foreign Key (RequestId) References Request(RequestId);
+Alter Table Requestskill Add Foreign Key (SkillId) References Skill(SkillId);
+Alter Table Review Add Foreign Key (RequestId) References Request(RequestId);
+Alter Table Review Add Foreign Key (AccountId) References Volunteer(AccountId);
+Alter Table Response Add Foreign Key (ResponderId) References Volunteer(AccountId);
+Alter Table Response Add Foreign Key (RequestId) References Request(RequestId);
+
+
