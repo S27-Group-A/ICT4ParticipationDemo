700949ab6e04da701f87c8f9a9d842dfec08efc9
Sven Henderickx <s.henderickx@student.fontys.nl>
2016-06-09 10:10:44 +0200

    Patient en volunteer properties en constructor

--- Participation ASP/Participation ASP/Models/Patient.cs
+++ Participation ASP/Participation ASP/Models/Patient.cs
@@ -5,7 +5,18 @@ using System.Web;
 
 namespace Participation_ASP.Models
 {
-    public class Patient
+    public class Patient : Account
     {
+        public bool Ov { get; set; }
+
+        public Patient(int accountId, string username, string password, string email, string name, int phone,
+            DateTime dateCancellation, string address, string location, bool hasCar, bool hasDriversLicense, string rfid,
+            bool banned, bool unban, bool enabled, bool ov)
+            : base(
+                accountId, username, password, email, name, phone, dateCancellation, address, location, hasCar,
+                hasDriversLicense, rfid, banned, unban, enabled)
+        {
+            this.Ov = ov;
+        }
     }
 }
