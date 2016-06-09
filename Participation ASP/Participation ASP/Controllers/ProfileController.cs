4b353298a822f3426b123ac188e33753df4112dc
Sven Henderickx <s.henderickx@student.fontys.nl>
2016-06-09 13:08:39 +0200

    peop

--- Participation ASP/Participation ASP/Controllers/ProfileController.cs
+++ Participation ASP/Participation ASP/Controllers/ProfileController.cs
@@ -12,8 +12,19 @@ namespace Participation_ASP.Controllers
         // GET: Profile
         public ActionResult Index()
         {
-            Volunteer volunteer = new Volunteer(1, "Henk", "test", "-", "Henk", 0, DateTime.Now, "0", "0", true, true, "0", false, true, true, DateTime.Now, "0", "0", true);
+            List<Availability> tempA = new List<Availability>();
+            List<Skill> tempS = new List<Skill>();
+            tempA.Add(new Availability("Ma", "Morgen"));
+            tempA.Add(new Availability("Di", "Middag"));
+            tempS.Add(new Skill("Timmeren"));
+            Volunteer volunteer = new Volunteer(1, "Henk", "test", "-", "Henk", 0, DateTime.Now, "0", "0", true, true, "0", false, true, true, DateTime.Now, "0", "0", true, tempA, tempS);
             return View(volunteer);
+        }
+
+        [HttpPost]
+        public ActionResult AddSkill(FormCollection collection, Account volunteer)
+        {
+            collection["skill"]
         }
     }
 }
