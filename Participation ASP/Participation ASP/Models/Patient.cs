d7c12abcaab1322fee2195e2f4e9a69c4eff1ec2
Sven Henderickx <s.henderickx@student.fontys.nl>
2016-06-09 10:44:13 +0200

    request klasse

--- Participation ASP/Participation ASP/Models/Patient.cs
+++ Participation ASP/Participation ASP/Models/Patient.cs
@@ -8,15 +8,17 @@ namespace Participation_ASP.Models
     public class Patient : Account
     {
         public bool Ov { get; set; }
+        public List<Request> Requests { get; set; }
 
         public Patient(int accountId, string username, string password, string email, string name, int phone,
             DateTime dateCancellation, string address, string location, bool hasCar, bool hasDriversLicense, string rfid,
-            bool banned, bool unban, bool enabled, bool ov)
+            bool banned, bool unban, bool enabled, bool ov, List<Request> requests)
             : base(
                 accountId, username, password, email, name, phone, dateCancellation, address, location, hasCar,
                 hasDriversLicense, rfid, banned, unban, enabled)
         {
             this.Ov = ov;
+            this.Requests = requests;
         }
     }
 }
