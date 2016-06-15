da5da1e7fb8a1fdb24b012780191f8d9c5ecc0a8
Sven Henderickx <s.henderickx@student.fontys.nl>
2016-06-15 20:21:03 +0200

    request

---
+++ Participation ASP/Participation ASP/Controllers/RequestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Participation_ASP.Models;
+
+namespace Participation_ASP.Controllers
+{
+    public class RequestController : Controller
+    {
+        // GET: Request
+        public ActionResult Index()
+        {
+            IAccount accountInlog = (IAccount) Session["Account"];
+            if (accountInlog is Patient)
+            {
+                return View(accountInlog);
+            }
+            return RedirectToAction("Index", "Error");
+        }
+    }
+}
