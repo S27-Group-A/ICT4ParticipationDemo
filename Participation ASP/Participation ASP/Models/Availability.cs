7c00582d26c7728589d3072736d86eaaa56a68eb
Sven Henderickx <s.henderickx@student.fontys.nl>
2016-06-16 23:53:39 +0200

    availability

--- Participation ASP/Participation ASP/Models/Availability.cs
+++ Participation ASP/Participation ASP/Models/Availability.cs
@@ -2,9 +2,20 @@
 {
     public class Availability
     {
+        /// <summary>
+        /// Gets or sets the day
+        /// </summary>
         public string Day { get; set; }
+        /// <summary>
+        /// gets or sets the timeofday
+        /// </summary>
         public string TimeOfDay { get; set; }
 
+        /// <summary>
+        /// Constructor with all the properties
+        /// </summary>
+        /// <param name="day"></param>
+        /// <param name="timeOfDay"></param>
         public Availability(string day, string timeOfDay)
         {
             this.Day = day;
