eb049e6f96263a0736ad22a820d0b5f13476f2a2
Svenpai <svenjanssen12321@gmail.com>
2016-06-19 16:46:18 +0200

    Fixed adminsystem language consistency issues

--- Participation ASP/Participation ASP/Models/Volunteer.cs
+++ Participation ASP/Participation ASP/Models/Volunteer.cs
@@ -5,6 +5,7 @@ namespace Participation_ASP.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Web;
 
